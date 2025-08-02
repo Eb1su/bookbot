56ed26e285c8edae91585b9472680ea5cce27665
Eb1su <higginsjamie3@gmail.com>
2025-08-03 00:40:14 +0100

    Added feature to list character count of a book contents

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import book_word_count
+from stats import book_char_count
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -10,7 +11,9 @@ def main():
     frankenstein_path = "./books/frankenstein.txt"
     book = get_book_text(frankenstein_path)
     word_count = book_word_count(book)
+    character_count = book_char_count(book)
     print(word_count)
+    print(character_count)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def book_word_count(book_contents):
     words = book_contents.split()
     return f"{len(words)} words found in the document"
+
+def book_char_count(book_contents):
+    char_list = {}
+    for char in book_contents:
+        if char.lower() not in char_list:
+            char_list[char.lower()] = 1
+        else:
+            char_list[char.lower()] += 1
+    return char_list
