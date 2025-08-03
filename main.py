63357e0d301c2257c7ed6b560f35384473d5b9b3
Eb1su <higginsjamie3@gmail.com>
2025-08-03 01:24:13 +0100

    Added bookbot report that lists word count and list of character count sorted from largest to smallest

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from stats import book_word_count
 from stats import book_char_count
+from stats import sort_char
 
 def get_book_text(book_path):
     with open(book_path) as f:
         book_contents = f.read()
         return book_contents
 
-
 def main():
-    frankenstein_path = "./books/frankenstein.txt"
-    book = get_book_text(frankenstein_path)
+    book_path = "./books/frankenstein.txt"
+    book = get_book_text(book_path)
     word_count = book_word_count(book)
     character_count = book_char_count(book)
-    print(word_count)
-    print(character_count)
+    sort_count = sort_char(character_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path[2:]}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count}")
+    print("--------- Character Count -------")
+    for entry in sort_count:
+        count = entry["num"]
+        char = entry["char"]
+        if char.isalpha() == True:
+            print(f"{char}: {count}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 def book_word_count(book_contents):
     words = book_contents.split()
-    return f"{len(words)} words found in the document"
+    return f"{len(words)} total words"
 
 def book_char_count(book_contents):
     char_list = {}
@@ -10,3 +10,17 @@ def book_char_count(book_contents):
         else:
             char_list[char.lower()] += 1
     return char_list
+
+def sort_char(unsorted_char):
+    list_of_char = []
+    for char in unsorted_char:
+        current_char = {}
+        current_char["char"] = char
+        current_char["num"] = unsorted_char[char]
+        list_of_char.append(current_char)
+    list_of_char.sort(reverse=True, key=sort_on)
+    return list_of_char
+    
+def sort_on(items):
+    return items["num"]
+    
