31cb4409900b8c8a7ea531cc7dea5419dcc7061d
Eb1su <higginsjamie3@gmail.com>
2025-08-03 02:06:44 +0100

    Updated output formatting and codebase to make it easier to read

--- main.py
+++ main.py
@@ -9,7 +9,20 @@ def get_book_text(book_path):
         book_contents = f.read()
         return book_contents
 
+def formatted_out(book_path, word_count, sorted_char):
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count}")
+    print("--------- Character Count -------")
+    for entry in sorted_char:
+        if char.isalpha() == True:
+            print(f"{entry["char"]}: {entry["num"]}")
+    print("============= END ===============")
+
 def main():
+    #If no file path has been provided return the following message
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
@@ -18,17 +31,7 @@ def main():
     word_count = book_word_count(book)
     character_count = book_char_count(book)
     sort_count = sort_char(character_count)
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
-    print("----------- Word Count ----------")
-    print(f"Found {word_count}")
-    print("--------- Character Count -------")
-    for entry in sort_count:
-        count = entry["num"]
-        char = entry["char"]
-        if char.isalpha() == True:
-            print(f"{char}: {count}")
-    print("============= END ===============")
-
+    
+    formatted_out(book_path, word_count, sort_count)
 
 main()
--- stats.py
+++ stats.py
@@ -4,6 +4,7 @@ def book_word_count(book_contents):
 
 def book_char_count(book_contents):
     char_list = {}
+
     for char in book_contents:
         if char.lower() not in char_list:
             char_list[char.lower()] = 1
@@ -13,11 +14,13 @@ def book_char_count(book_contents):
 
 def sort_char(unsorted_char):
     list_of_char = []
+
     for char in unsorted_char:
         current_char = {}
         current_char["char"] = char
         current_char["num"] = unsorted_char[char]
         list_of_char.append(current_char)
+
     list_of_char.sort(reverse=True, key=sort_on)
     return list_of_char
     
